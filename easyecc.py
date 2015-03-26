9666ffc9108e296744fbc9ed993e92e4452fa699
Andrew Wason <rectalogic@rectalogic.com>
2015-03-26 10:59:27 -0400

    PEP8 fixes.

--- easyecc.py
+++ easyecc.py
@@ -1,7 +1,7 @@
 import _easyecc
 
-class ECC(object):
 
+class ECC(object):
     def __init__(self, *args, **kwargs):
         self._private_key = kwargs.get('private_key')
         self._public_key = kwargs.get('public_key')
@@ -9,7 +9,7 @@ class ECC(object):
     @classmethod
     def new_key(cls):
         private_key, public_key = _easyecc.new_key()
-        return cls(private_key=private_key, public_key = public_key)
+        return cls(private_key=private_key, public_key=public_key)
 
     def encrypt(self, plaintext):
         return _easyecc.encrypt(self._public_key, plaintext)
@@ -18,7 +18,6 @@ class ECC(object):
         return _easyecc.decrypt(self._private_key, ciphertext)
 
 if __name__ == "__main__":
-    
     plaintext = "This is a big secret"
     print plaintext
     ecc = ECC.new_key()
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup, Extension
 
 BOOST_LIB = 'boost_python'
 if sys.platform == 'darwin':
-	BOOST_LIB = 'boost_python-mt'
+    BOOST_LIB = 'boost_python-mt'
 
 base_modules = [
     Extension('_easyecc', [
-            'pyeasyecc.cpp',
+        'pyeasyecc.cpp',
         ],
         libraries=['cryptopp', BOOST_LIB],
         extra_compile_args=['-fPIC'])
@@ -20,15 +20,13 @@ packages = ['easyecc']
 modules = filter(lambda m: reduce(lambda x, y: x and os.path.exists(y), m.depends, True), base_modules)
 missing_modules = filter(lambda m: m not in modules, base_modules)
 if missing_modules:
-	print 'WARNING: Some Python modules are missing dependencies: %s' % ', '.join(map(lambda x: x.name, missing_modules))
+    print 'WARNING: Some Python modules are missing dependencies: %s' % ', '.join(map(lambda x: x.name, missing_modules))
 
 setup(
-    name = 'easyecc',
-    description = '''A simple wrapper around Crypto++ for Elliptical Curve Cryptography''',
-    version = '0.1',
-    author = 'Alex Khomenko',
-    author_email = 'khomenko@cs.stanford.edu',
-    ext_modules=modules, 
+    name='easyecc',
+    description='''A simple wrapper around Crypto++ for Elliptical Curve Cryptography''',
+    version='0.1',
+    author='Alex Khomenko',
+    author_email='khomenko@cs.stanford.edu',
+    ext_modules=modules,
     py_modules=['easyecc'])
-
-
